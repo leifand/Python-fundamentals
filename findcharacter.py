cf249f82a04bc98ef3187a7f60a098adc09170e4
Leif Anderson <leif.v.anderson@gmail.com>
2017-07-05 13:11:07 -0500

    completed find character exercise

--- findcharacter.py
+++ findcharacter.py
@@ -3,8 +3,21 @@
     Leif Anderson 7/5/17
 '''
 
-def findCharacter(char, stringArray):
-    
+
+def containsCharacter(char, stringArray):
+# returns a list of strings containing the passed in character
+    arrayOut = []
+    for i in range(0, len(stringArray)):
+        contains = False
+        for j in range(0, len(stringArray[i])):
+            if char == stringArray[i][j]:
+                contains = True
+        if contains:
+            arrayOut.append(stringArray[i])
+    return arrayOut
+
 
 word_list = ['hello','world','my','name','is','Anna']
 char = 'o'
+
+print containsCharacter(char, word_list)
