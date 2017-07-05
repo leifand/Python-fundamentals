d490d9e7f1d98e030e9779178881798a7859c1fe
Leif Anderson <leif.v.anderson@gmail.com>
2017-07-05 18:15:50 -0500

    working on multiplication exercise

--- deckofcards.py
+++ deckofcards.py
@@ -16,9 +16,36 @@ class Card(object):
 
 class Deck(object):
     def __init__(self):
+        self.cardnames = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
         self.cards = []
-        
+        for i in range(0, 4):
+            for j in range(0, 10):
+                if i == 0:
+                    self.cards.append(Card("Hearts", self.cardnames[j], j+1, "red"))
+                elif i == 1:
+                    self.cards.append(Card("Diamonds", self.cardnames[j], j+1, "red"))
+                elif i == 2:
+                    self.cards.append(Card("Spades", self.cardnames[j], j+1, "black"))
+                elif i == 3:
+                    self.cards.append(Card("Clubs", self.cardnames[j], j+1, "black"))
+            for j in range(10, 13):
+                if i == 0:
+                    self.cards.append(Card("Hearts", self.cardnames[j], 10, "red"))
+                elif i == 1:
+                    self.cards.append(Card("Diamonds", self.cardnames[j], 10, "red"))
+                elif i == 2:
+                    self.cards.append(Card("Spades", self.cardnames[j], 10, "black"))
+                elif i == 3:
+                    self.cards.append(Card("Clubs", self.cardnames[j], 10, "black"))
 
+x = Deck()
+print x
+for z in x.cards:
+    print z
+print len(x.cards)
+
+
+'''
 deal1 = [
 Card("Ace",1),
 Card("Two", 2),
@@ -40,3 +67,4 @@ print d
 print d.cards[0]
 
 #print __str__(array1)
+'''
