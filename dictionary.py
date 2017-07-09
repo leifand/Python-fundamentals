1ae5b670cdf628af9637c5e05dce233dd4fc5c19
Leif Anderson <leif.v.anderson@gmail.com>
2017-07-08 20:20:25 -0500

    modified dictionary for names exercise

--- dictionary.py
+++ dictionary.py
@@ -23,6 +23,10 @@ def printDictionary(dictionary):
     for i in range(0, keyslen):
         print keys[i] + ' : ' + dictionary[keys[i]]
 
+# modified for the names exercise ...
+def nprintDictionary(dojo_member):
+    return dojo_member['fname'] + ' ' + dojo_member['lname'] + ' ' + str(len(dojo_member['fname']) + len(dojo_member['lname']))
+
 def printDojo(nested_dict):
     keys = nested_dict.keys()
     keys.reverse()
@@ -34,4 +38,18 @@ def printDojo(nested_dict):
         for j in range(0, lstlen):
             printDictionary(lst[j])
 
+# modified for names exercise
+def nprintDojo(nested_dict):
+    keys = nested_dict.keys()
+    keys.reverse()
+    keyslen = len(keys)
+    for i in range(0, keyslen):
+        print keys[i] + ' '
+        lst = nested_dict[keys[i]]
+        lstlen = len(lst)
+        for j in range(0, lstlen):
+            print str(j+1) + ' - ' + nprintDictionary(lst[j])
+
 printDojo(dojo)
+print '>>>>>>>>>>>>>>>>'
+nprintDojo(dojo)
